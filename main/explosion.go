ee25100c874a456f648daea40f040d4dacb3255b
Adam Westman <adam.westman@king.com>
2017-03-19 11:20:24 +0100

    block_deselect Animate Added particlefx

---
+++ main/explosion.go
@@ -0,0 +1,30 @@
+components {
+  id: "particlefx"
+  component: "/main/block_destroyed.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "script"
+  component: "/main/explosion.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
